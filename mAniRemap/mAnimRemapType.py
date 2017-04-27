a6a27aa2175a4c54f00f40303834fa70845fbbcc
davidpower <david962041@gmail.com>
2017-04-27 14:51:44 +0800

    remove filp test code

--- mAniRemap/mAnimRemapType.py
+++ mAniRemap/mAnimRemapType.py
@@ -8,7 +8,7 @@ def remapNameMatch(src, dis):
 	"""
 	def rmNS(dagPath):
 		''' Flip TEST '''
-		#'''
+		'''
 		dp = []
 		for dag in dagPath.split('|'):
 			cd = dag.split(':')[-1]
@@ -19,9 +19,10 @@ def remapNameMatch(src, dis):
 				fd = cd[:-2] + '_R'
 			dp.append(fd)
 		return '|'.join(dp)
+		'''
 		
 		''' clean up src namespace and leave basename '''
-		#return '|'.join([ dag.split(':')[-1] for dag in dagPath.split('|') ])
+		return '|'.join([ dag.split(':')[-1] for dag in dagPath.split('|') ])
 
 	keyMap = {}
 	for ctrl in src:
